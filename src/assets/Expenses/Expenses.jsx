933d954dbce380d5955bdd8c8c7c769b4829ac88
Carl-Robert <robertm6ts@gmail.com>
2025-02-12 11:29:45 +0200

    yl12 Andmete suunamine(bottom-up) ja vaartuse hetkeseisu lugemine ja maaramine

--- src/assets/Expenses/Expenses.jsx
+++ src/assets/Expenses/Expenses.jsx
@@ -1,14 +1,23 @@
+import { useState } from 'react'
 import ExpenseItem from './ExpenseItem.jsx'
 import Card from '../UI/Card.jsx'
 import './Expenses.css'
-
+import ExpensesFilter from './ExpensesFilter.jsx'
 
 
 
 const Expenses = (props) => {
+  const [filteredYear, setFilteredYear] = useState('2025')
+
+    const filterChangeHandler = (selectedYear) => { 
+      setFilteredYear(selectedYear)
+        console.log(`Year data in Expenses.jsx ${selectedYear}` )
+    }
 
   return (
     <Card className='expenses'>
+        <ExpensesFilter selectedYear={filteredYear} onChangeFilter={filterChangeHandler} />
+        
         <ExpenseItem expenseData={props.expenses[0]}/>
         <ExpenseItem expenseData={props.expenses[1]}/>
     </Card>
